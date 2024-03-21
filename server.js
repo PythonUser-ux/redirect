fbef41fe9c81854f195196726368583033a101c2
Andrea Virgillito <71944456+PythonUser-ux@users.noreply.github.com>
2024-03-21 10:17:49 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const cors = require("cors"); // to resolve CORS (Cross-Origin Resource Sharing) Issues // CORS enhances security by preventing unintended cross-origin requests. Without CORS, browsers restrict cross-origin requests by default.
+const corsOptions = require("./config/corsOption");
+const redirect = require("./middleware/redirectMiddleware");
+
+const PORT = process.env.PORT || 3500;
+
+app.use(cors(corsOptions)); // we put this line as soon as possible but after *app.use(logger);*
+// by using this now I can run "fetch('http://localhost:3500');" from dev tools in more websites than before the introduction of this line
+
+app.all("*", redirect);
+
+// app.use(function (err, req, res, next) {
+//     console.error(err.stack);
+//     res.status(500).send(err.message);
+// })
+
+// Export the Express API
+module.exports = app;
